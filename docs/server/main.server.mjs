9b2f7d481baae9357032772c1495c3a529b486a8
Athlaes <chartierlucas@outlook.fr>
2024-01-25 14:00:15 +0100

    commit good changes

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-XKSIGFFB.mjs";import"./chunk-T5LWD2O5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { MapComponent } from './components/map/map.component';
 
-export const routes: Routes = [
-  {path:"map", component: MapComponent}
-];
+export const routes: Routes = [];
