b2fd7a42842bba1d1f71b2b26811f963aa9ade98
Athlaes <chartierlucas@outlook.fr>
2024-02-08 01:16:48 +0100

    changed navigation added routerlink in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MapComponent } from './components/map/map.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  {path: "", component: MapComponent}
+  {path: "", component: HomeComponent},
+  {path: "map", component: MapComponent}
 ];
