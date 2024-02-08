e9bb45095c735d8379dbc75708ca8678f36cdcd5
Athlaes <chartierlucas@outlook.fr>
2024-02-08 01:34:16 +0100

    fix failing TU

--- src/app/components/shared/navbar/navbar.component.spec.ts
+++ src/app/components/shared/navbar/navbar.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterLink, RouterModule } from '@angular/router';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -9,7 +10,7 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent, BrowserAnimationsModule]
+      imports: [NavbarComponent, BrowserAnimationsModule, RouterLink, RouterModule.forRoot([])]
     })
     .compileComponents();
 
